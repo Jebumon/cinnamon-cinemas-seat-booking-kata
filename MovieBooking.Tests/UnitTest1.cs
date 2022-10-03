d7f454ab66ad52fe6535798a373e722f25346e8f
Jebumon <jebumon@gmail.com>
2022-10-03 22:28:30 +0100

    Initial Commit

---
+++ MovieBooking.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MovieBooking.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
